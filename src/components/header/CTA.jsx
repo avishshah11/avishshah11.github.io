157273f59d92a71e4a083e85e6c65c050b353492
avishshah11 <avishshah1108@gmail.com>
2024-10-19 21:35:38 +0530

    Adding changes

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import Resume from '../../assets/Avish_Shah_Resume.pdf'
+import React from "react";
+import Resume from "../../assets/Avish_Shah_Resume.pdf";
 
 const CTA = () => {
   const handleDownload = () => {
+    const link = document.createElement("a");
 
-    const link = document.createElement('a')
+    link.href = Resume;
 
-    link.href = Resume
+    link.download = "Resume";
 
-    link.download = "Resume"
+    document.body.appendChild(link);
 
-    document.body.appendChild(link)
+    link.click();
 
-    link.click()
-
-    document.body.removeChild(link)
-
-    setTimeout(() => URL.revokeObjectURL(link.href), 7000);
-
-  }
+    document.body.removeChild(link);
+  };
   return (
-    <div className='cta'>
-        <button onClick={handleDownload} className='btn btn-primary'>Resume</button>
-        <a href="#contact" className="btn btn-primary">Let's Talk</a>
+    <div className="cta">
+      <button onClick={handleDownload} className="btn btn-primary">
+        Resume
+      </button>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
